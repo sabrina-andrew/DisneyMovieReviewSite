12ba737303624c677108403854bf8cd824405679
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-02-21 10:51:14 -0500

    Some files set up and started list

--- DisneyMovieReviewSite.Tests/HomeControllerTests.cs
+++ DisneyMovieReviewSite.Tests/HomeControllerTests.cs
@@ -1,14 +1,19 @@
+using DisneyMovieReviewSite.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 
 namespace DisneyMovieReviewSite.Tests
 {
     public class HomeControllerTests
-    {
+    {       
         [Fact]
-        public void Test1()
+        public void Index_Has_A_Review()
         {
+            var underTest = new HomeController();
+            var result = underTest.Index();
 
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
