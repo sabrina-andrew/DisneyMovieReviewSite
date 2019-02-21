8fd5c2530a0753f9cef91ca751bf083a33cf570e
Alden <aj.molina092@gmail.com>
2019-02-21 15:09:55 -0500

    Added properties and fixed images

--- DisneyMovieReviewSite/Models/Movie.cs
+++ DisneyMovieReviewSite/Models/Movie.cs
@@ -11,5 +11,8 @@ namespace DisneyMovieReviewSite.Models
         public string Name { get; set; }
         public int ID { get; set; }
         public string Review { get; set; }
+        public string Content { get; set; }
+        public string Category { get; set; }
+        public string ImgPath { get; set; }
     }
 }
