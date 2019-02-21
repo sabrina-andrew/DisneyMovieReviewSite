a58b066978d4ee661ed3cb8e7978e9ba4f84ad63
Alden <aj.molina092@gmail.com>
2019-02-21 17:32:18 -0500

    Added navbar style structure.

--- DisneyMovieReviewSite/Models/Movie.cs
+++ DisneyMovieReviewSite/Models/Movie.cs
@@ -10,6 +10,9 @@ namespace DisneyMovieReviewSite.Models
     {
         public string Name { get; set; }
         public int ID { get; set; }
-        public string Review { get; set; }
+        public string Content { get; set; }
+        public string Rating { get; set; }
+        public string Category { get; set; }
+        public string ImgPath { get; set; }
     }
 }
