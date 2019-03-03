26807a9643e3da39dcf29cd57fda8cc2f49857c4
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-03-03 17:34:54 -0500

    Set up Reviews Index and Details Forgot to switch branches to work on create movie and edit movie

--- DisneyMovieReviewSite/Models/Review.cs
+++ DisneyMovieReviewSite/Models/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace DisneyMovieReviewSite.Models
 {
     public class Review
     {
+        
         public int ReviewID { get; set; }
         public int MovieID { get; set; }
         public virtual Movie Movie { get; set; }
