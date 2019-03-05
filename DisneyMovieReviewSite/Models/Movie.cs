ef1d4306dd99d8243a764e016a083671e007ef97
Alden Molina <ajd029@yahoo.com>
2019-03-05 01:06:47 -0500

    Added average rating

--- DisneyMovieReviewSite/Models/Movie.cs
+++ DisneyMovieReviewSite/Models/Movie.cs
@@ -11,7 +11,7 @@ namespace DisneyMovieReviewSite.Models
         public int MovieID { get; set; }
         public string Name { get; set; }
         public string Synopsis { get; set; }
-        public double Rating { get; set; }
+        public decimal AverageRating { get; set; }
         public string ImgPath { get; set; }
         public int ReleaseYear { get; set; }
         public int RunTime { get; set; }
@@ -20,5 +20,31 @@ namespace DisneyMovieReviewSite.Models
         public virtual Category Category { get; set; }
         public virtual List<Review> Reviews { get; set; }
 
+        public decimal AverageCalculation()
+        {   
+            int totalRating = 0;
+            foreach (var review in Reviews)
+            {
+                totalRating += review.UserRating;
+            }
+
+            decimal averageRating = totalRating / Reviews.Count;
+            
+            decimal roundRating = Math.Round(averageRating);
+
+            AverageRating = roundRating;
+
+            return AverageRating;
+
+        }
+
+
+
+
     }
+
+
+    
 }
+
+
